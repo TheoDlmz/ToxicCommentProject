8e5a8aca2c51813df1bc29f90fa7d655758d2f9c
Théo Delemazure <34279251+TheoDlmz@users.noreply.github.com>
2020-03-09 09:30:38 +0100

    Update bow.py

--- bow.py
+++ bow.py
@@ -49,7 +49,6 @@ print(txtidx[0])
 
 vocab_size = len(vocab)
 vector_size = 100
-
 embed = nn.Embedding(vocab_size, vector_size)
 
 ##
@@ -57,3 +56,55 @@ lookup_tensor = torch.tensor([vocab["hello"]], dtype=torch.long)
 hello_embed = embed(lookup_tensor)
 print(hello_embed)
 
+##
+
+labl = torch.from_numpy(labels).float()
+
+## FIRST MODEL
+
+class CBOW_classifier(nn.Module):
+
+    def __init__(self, vocab_size, embedding_dim):
+        super(CBOW_classifier, self).__init__()
+        self.embeddings = nn.Embedding(
+                num_embeddings=vocab_size,
+                embedding_dim=embedding_dim)
+        self.lin = nn.Linear(embedding_dim,6)
+        self.activation = nn.Sigmoid()
+
+    def forward(self, inputs):
+        out = self.embeddings(inputs)
+        out = out.sum(dim=0)
+        return self.activation(self.lin(out))
+        #return self.lin(out)
+        
+        
+##
+
+classifier = CBOW_classifier(vocab_size=vocab_size,embedding_dim=40)
+print(classifier(txtidx[1]))
+
+##
+
+loss_function = nn.MultiLabelSoftMarginLoss()
+optimizer = torch.optim.Adagrad(classifier.parameters(),lr=1e-1)
+
+##
+
+
+total = int(100)
+randomidx = list(range(total))
+preds = torch.zeros((total,6))
+
+for epoch in range(10):
+    total_loss = torch.Tensor([0])
+    random.shuffle(randomidx)
+    for index in randomidx:
+        x = txtidx[index]
+        classifier.zero_grad()
+        probs = classifier(x)
+        loss = loss_function(probs[None,:], labl[index][None,:])
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.data
+    print(epoch, total_loss[0])
